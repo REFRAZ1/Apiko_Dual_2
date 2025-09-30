019d0c44a77605aa9b2b1ae95f1b1bb057ae4c94
REFRAZ1 <refraz1so2@gmail.com>
2025-09-30 23:54:47 +0300

    second commit

--- script4.js
+++ script4.js
@@ -1,36 +1,73 @@
-function calculateWordsInString(string) {
+//БУЛО ЗРОБЛЕНО 2 ВАРІАНТИ ВИКОНАННЯ 4 ЗАВДАННЯ
 
-    const text = string.trim();
+        // 1 ВАРІАНТ ВИКОНАННЯ 4 ЗВАДАННЯ
 
-    let strCopy = [];
-    strCopy = text.split(" ");
+// function calculateWordsInString(string) {
 
-    console.log(strCopy);
+//     const text = string.trim();
 
-    let tapeLenght = 0;
+//     let strCopy = [];
+//     strCopy = text.split(" ");
 
-    const ADD_COUNTER_FOR_LENGHT = 1;
-    const MINUS_UNDEFIEND = -1;
+//     console.log(strCopy);
 
-    for (let i = 0; i <= strCopy.length; i++) {
+//     let tapeLenght = 0;
 
-        if (strCopy[i] !== '') {
-            tapeLenght += ADD_COUNTER_FOR_LENGHT;
-            console.log(strCopy[i]);
-        }
-        else {
-            continue;
-        }
+//     const ADD_COUNTER_FOR_LENGHT = 1;
+//     const MINUS_UNDEFIEND = -1;
 
-    }
+//     for (let i = 0; i <= strCopy.length; i++) {
 
-    tapeLenght += MINUS_UNDEFIEND;
-    console.log(tapeLenght) 
-    return;
-}
+//         if (strCopy[i] !== '') {
+//             tapeLenght += ADD_COUNTER_FOR_LENGHT;
+//             console.log(strCopy[i]);
+//         }
+//         else {
+//             continue;
+//         }
+
+//     }
+
+//     tapeLenght += MINUS_UNDEFIEND;
+//     console.log(tapeLenght) 
+//     return;
+// }
 
 
 // calculateWordsInString("Easy string for count");
 // calculateWordsInString("Easy");
 // calculateWordsInString("Some string with a triple   space");
 // calculateWordsInString("Some?  string, with a triple   space");
+
+
+        // 2 ВАРІАНТ ВИКОНАННЯ 4 ЗВАДАННЯ
+
+function calculateWordsInStringTwo(string){
+    //Забираю пробіли на початку і в кінці
+    const text = string.trim();
+
+    let tapeLenght = 1;
+
+    for (let i = 0; i < text.length; i++) {
+
+        if (text[i] === " " && text[i + 1] === " ") {
+
+            continue;
+
+        }
+        else if (text[i] === " " && text[i + 1] !== " ") {
+
+            tapeLenght++;
+
+        }
+    }
+
+    console.log(tapeLenght);
+    return;
+        
+}
+
+calculateWordsInStringTwo("Easy string for count");
+calculateWordsInStringTwo("Easy");
+calculateWordsInStringTwo("Some string with a triple   space");
+calculateWordsInStringTwo("Some?  string, with a triple   space");
